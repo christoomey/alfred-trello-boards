d5c795890ff142f1648c8c5afbcf8f2fc7817e36
Chris Toomey <chris@ctoomey.com>
2013-10-11 14:12:15 -0400

    Expose workflow for caching, improve error handling

--- boards.rb
+++ boards.rb
@@ -1,11 +1,72 @@
 require 'restclient'
+require 'dotenv'
 require 'json'
-require 'yaml'
 
-Board = Struct.new(:id, :name, :description, :url) do
-  def to_json
-    Hash[self.each_pair.to_a].to_json
+def main
+  log "Begin trello board caching run"
+  username, developer_public_key, member_token = load_credentials
+  log "Credentials loaded from env"
+  fetch_url = boards_url(username, developer_public_key, member_token)
+  log "Fetching with URL [#{fetch_url}]"
+  boards = fetch_trello_boards(fetch_url)
+  log "Fetched [#{boards.count}] urls"
+  boards_data = simplified_boards_data(open_baords(boards))
+  write_boards_data_cache_file(boards_data)
+  puts "Boards cached successfully."
+end
+
+def write_boards_data_cache_file(boards_data)
+  open('boards.json', 'w') {|f| f.write(boards_data.to_json) }
+end
+
+def fetch_trello_boards(url)
+  resp = RestClient.get(url)
+  boards = JSON.parse(resp)
+rescue RestClient::Unauthorized
+  auth_msg = "Unauthorized error. Unable to fetch from API. Exiting"
+  log auth_msg
+  puts auth_msg
+  exit 1
+end
+
+def boards_url(username, developer_public_key, member_token)
+  api_base_url = 'https://api.trello.com/1/'
+  url = "#{api_base_url}members/#{username}/boards?key=#{developer_public_key}&token=#{member_token}"
+end
+
+def open_baords(boards)
+  boards.select {|board| !board['closed'] }
+end
+
+def simplified_boards_data(boards)
+  boards.map { |board| board.slice('id', 'name', 'desc', 'url') }
+end
+
+def load_credentials
+  home_directory = File.expand_path('~/')
+  Dir.chdir(home_directory) { Dotenv.load }
+  username = ENV['TRELLO_USERNAME']
+  developer_public_key = ENV['TRELLO_DEVELOPER_KEY']
+  token = ENV['TRELLO_TOKEN']
+  credentials = [username, developer_public_key, token]
+  unless credentials.all?
+    credentials_missing_msg = "Credentials not found. Exiting.\n[expected in ~/.env]. See README"
+    log credentials_missing_msg
+    puts credentials_missing_msg
+    exit 1
   end
+  credentials
+end
+
+def log(msg)
+  @file ||= open_logger_file
+  @file << msg
+  @file << "\n"
+  @file.flush
+end
+
+def open_logger_file
+  File.open('log.txt', 'w')
 end
 
 class Hash
@@ -15,31 +76,6 @@ class Hash
     keys.each { |k| hash[k] = self[k] if has_key?(k) }
     hash
   end
-end
-
-def main
-  trello = authenticated_trello_client
-  open('boards.json', 'w') {|f| f.write(trello.to_json) }
-end
-
-def authenticated_trello_client
-  api_base_url = 'https://api.trello.com/1/'
-  username = 'ctoomey'
-  developer_public_key = 'c2a20b6b4c6433d26a9878f6a42b8db7'
-  member_token = '44267ceee0bf7a08443ceeaefbf1cf5b64b701140af721dbbbd19c03f870608b'
-  url = "#{api_base_url}members/#{username}/boards?key=#{developer_public_key}&token=#{member_token}"
-  resp = RestClient.get url
-  boards = JSON.parse resp
-  mine = myboards(open_baords(boards))
-end
-
-def open_baords(boards)
-  boards.select {|board| !board['closed'] }
-end
-
-def myboards(boards)
-  # boards.map { |board| Board.new(board['id'], board['name'], board['desc'], board['url']) }
-  boards.map { |board| board.slice('id', 'name', 'desc', 'url') }
 end
 
 main
